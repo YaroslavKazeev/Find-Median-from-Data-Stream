987cb303e5fc9a89739751f4722e0dbaf1fce406
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-06-09 21:32:23 +0200

    feat: initialize MedianFinder class with method stubs for data stream processing

---
+++ Gemini_3.1_Pro_solution copy.py
@@ -0,0 +1,12 @@
+class MedianFinder:
+    def __init__(self):
+        # write your code here
+        pass
+
+    def addNum(self, num: int) -> None:
+        # write your code here
+        pass
+
+    def findMedian(self) -> float:
+        # write your code here
+        pass
